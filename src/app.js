e37a47259cf579fe172ecae1690bf64757f31895
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2023-11-26 21:30:45 +0000

    Arreglado por enesima vez

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   const name = ["caracas", "edimburgh", "Madrid"];
   const domain = [".com", ".uk", ".es"];
