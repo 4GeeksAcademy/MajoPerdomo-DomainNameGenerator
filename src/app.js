9838aec73789c77ba94b4bf6bee66e4bf9fd7280
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-10-25 09:53:42 +0000

    DG Ft console log incluido

--- src/app.js
+++ src/app.js
@@ -9,25 +9,24 @@ const adj = ["beauty", "gorgeous", "brilliant", "fancy", "smart"];
 const noun = ["city", "purse", "jacket", "cat", "elephant"];
 const dom = [".com", ".es", ".uk", ".it"];
 
-function getRandom(max) {
-    return Math.floor(Math.random() * max);
-}
+function generateDomains() {
+    const domainList = [];
 
+    for (let i = 0; i < pronoun.length; i++) {
+        for (let j = 0; j < adj.length; j++) {
+            for (let k = 0; k < noun.length; k++) {
+                for (let l = 0; l < dom.length; l++) {
+                    const domainName = `${pronoun[i]}${adj[j]}${noun[k]}${dom[l]}`;
+                    domainList.push(domainName);
+                    console.log(domainName);
+                }
+            }
+        }
+    }
 
-function createDomain() {
-    const domain = 
-        pronoun[getRandom(pronoun.length)] +
-        adj[getRandom(adj.length)] +
-        noun[getRandom(noun.length)] +
-        dom[getRandom(dom.length)];
-    
-  
-    console.log("Dominio generado: " + domain);
-    
-  
-    document.getElementById("domain").innerHTML = domain;
+    return domainList;
 }
 
 window.onload = function() {
-    createDomain();
-};
+    const domains = generateDomains();
+       };
