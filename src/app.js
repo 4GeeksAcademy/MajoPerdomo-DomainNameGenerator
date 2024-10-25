76f29a6d0a81bd9a29a4dfb6dce9aaf9959393bf
Majo Perdomo <141275874+majoperdomo@users.noreply.github.com>
2024-10-25 09:39:28 +0000

    Domain generator

--- src/app.js
+++ src/app.js
@@ -4,40 +4,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-/*
+const pronoun = ["The", "our", "your"];
+const adj = ["beauty", "gorgeous", "brilliant", "fancy", "smart"];
+const noun = ["city", "purse", "jacket", "cat", "elephant"];
+const dom = [".com", ".es", ".uk", ".it"];
+
+function getRandom(max) {
+    return Math.floor(Math.random() * max);
+}
+
+
+function createDomain() {
+    const domain = 
+        pronoun[getRandom(pronoun.length)] +
+        adj[getRandom(adj.length)] +
+        noun[getRandom(noun.length)] +
+        dom[getRandom(dom.length)];
+    
+  
+    console.log("Dominio generado: " + domain);
+    
+  
+    document.getElementById("domain").innerHTML = domain;
+}
+
 window.onload = function() {
-  const name = ["caracas", "edimburgh", "Madrid"];
-  const domain = [".com", ".uk", ".es"];
-
-  name.forEach(el => {
-    domain.forEach(el2 => {
-      console.log(el + el2);
-    });
-  });
+    createDomain();
 };
-*/
-
-/*
-window.onload = function() {
-	createDomain();	
-};
-
-let pronoun = ['The','our','your'];
-let adj = ['beauty', 'gorgeous','brilliant','fancy','smart' ];
-let noun = ['city','purse','jacket','cat','elefant'];
-let dom = ['.com','.es','.uk','.it'];
-function getRandom(range) {
-	return Math.floor(Math.random() * range);
-};
-
-function createDomain(){
-	let domain =
-	pronoun[getRandom(pronoun.length)] + 
-	adj[getRandom(adj.length)] + 
-	noun[getRandom(noun.length)] +
-	dom[getRandom(dom.length)];
-
-	document.getElementById("domain").innerHTML = domain;
-};
-*/
-
